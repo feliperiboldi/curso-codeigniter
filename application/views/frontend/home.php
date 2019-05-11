a388d7499a35f16210c4659dbbfde33405e9d981
Felipe Riboldi <feliperiboldimarques@outlook.com>
2019-05-11 20:59:21 -0300

    Helper para formatação de data

--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -19,7 +19,7 @@
             <p class="lead">
                 por <a href="index.php">Start Bootstrap</a>
             </p>
-            <p><span class="glyphicon glyphicon-time"></span> Postado em 25 de Janeiro de 2017 10:00</p>
+            <p><span class="glyphicon glyphicon-time"></span><?= postadoem($destaque->data) ?></p>
             <hr>
             <img class="img-responsive" src="http://placehold.it/900x300" alt="">
             <hr>
